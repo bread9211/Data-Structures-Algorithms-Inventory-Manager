d90b35911f8cde2fb553c4f452afc0c37ee0e199
AverageCoder2 <107382107+AverageCoder2@users.noreply.github.com>
2026-02-02 10:53:02 -0800

    hopefully nothing is exploding?

---
+++ WarehouseManager.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class WarehouseManager {
+    private List<Warehouse> warehouses;
+    private List<ItemID> stockedIDs;
+    private Warehouse current;
+    private int nextSKU;
+
+    public WarehouseManager() {
+        warehouse = new ArrayList<>();
+        stockedIDs = new ArrayList<>();
+        current = null;
+        nextSKU = 0;
+    }
+
+    public void addWarehouse(String name) {
+        Warehouse w = new Warehouse(name);
+        warehouses.add(w);
+        current = w;
+    }
+    public void addOwned(Item item){
+        current.add(item,stockedIDs.get(item.getSKU()));
+    }
+    public void addNew(Item item, ItemID itemID){
+        itemID.setSKU(nextSKU);
+        item.setSKU(nextSKU);
+        nextSKU++;
+        stockedIDs.add(itemID);
+        current.add(item,itemID);
+    }
+    public itemID(int SKU){
+        return stockedIDs.get(SKU);
+    }
+    public void printAllWarehouses() {
+        for (Warehouse w : warehouse) {
+            w.printInventory();
+            System.out.println();
+        }
+    }
+}
